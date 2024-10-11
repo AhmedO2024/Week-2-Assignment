fc55c8dd97f656475f32e14c870b1ccb66f004c7
Ahmed Osman <a.osman2024@gmail.com>
2024-10-11 23:54:04 +0100

    week 2 assignment complete

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Week-2-Assignment
+
 Week 2 Assignment
+The images didn;t come out so great when they become the background, so this is one of the things that could be improved. I also wanted to add a stop sign to stop the back ground music as it can become annoying on a page if you donlt like the music or it takes up to much space but I had troubl with this so I didnt include it.
---
+++ app.js
@@ -0,0 +1,47 @@
+const images = [
+  "./images/antelope.jpg",
+  "./images/elephant.jpg",
+  "./images/lion.jpg",
+  "./images/ostrich.jpg",
+  "./images/wildebeest.jpg",
+  "./images/zebra.jpg",
+];
+const thumbnailContainer = document.getElementById("thumbnail-container");
+let currentIndex = 0;
+
+function updateBackground() {
+  document.body.style.backgroundImage = `url('${images[currentIndex]}')`;
+}
+
+images.forEach((src, index) => {
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = `Thumbnail of ${src.split("/").pop().split(".")[0]}`;
+  img.classList.add("thumbnail");
+
+  img.addEventListener("click", () => {
+    currentIndex = index;
+    updateBackground();
+    backgroundMusic.play();
+  });
+
+  thumbnailContainer.appendChild(img);
+});
+
+updateBackground();
+
+const backgroundMusic = document.getElementById("background-music");
+
+document.addEventListener("click", () => {
+  backgroundMusic.play();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateBackground();
+  } else if (event.key === "ArrowLeft") {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateBackground();
+  }
+});
